c5e461005200d933926dbec1b1f477168fd9d021
Lilian <lili.denisse2@gmail.com>
2023-03-29 22:43:10 -0300

    Prueba final

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import validator from './validator.js';
 console.log(validator);
 
 // Obtener los elementos del formulario
-const formularioTarjeta = document.getElementById("formulario")
+
 const nombre = document.getElementById('inputname');
 const numeroTarjetaInput = document.getElementById('numero-tarjeta');
 const mesInput = document.getElementById("inputmes");
-const yearInput = document.getElementById("inputano");
-const cvvInput = document.getElementById("inputcvc");
+//const yearInput = document.getElementById("inputano");
+//const cvvInput = document.getElementById("inputcvc");
 const verificarTarjetaButton = document.getElementById('verificar-tarjeta');
 
 // obtener los elementos para la tarjeta
-const mascara = document.getElementById("tarjeta-numero");
+//const mascara = document.getElementById("tarjeta-numero");
 const parrafo = document.getElementById('tarjeta-numero');
 
 const inputNombre = document.querySelector("#inputname");
@@ -25,14 +25,13 @@ const inputCvv = document.querySelector("#inputcvc");
 const cardName = document.querySelector("#tarjeta-nombre");
 const cardNumber = document.querySelector("#tarjeta-numero");
 const cardMes = document.querySelector("#tarjeta-mes");
-const cardYear = document.querySelector("#tarjeta-year");
+const cardYear = document.querySelector("#tarjeta-year"); 
 const cardCvv = document.querySelector("#tarjeta-cvv");
-const form = document.querySelector("#formulario")
 
 
 
-// Darle funcion a la tarjeta
-     // funcion para el nombre
+
+// Darle funcion a la tarjeta   funcion para el nombre    
 inputNombre.addEventListener("input", () => {
   cardName.innerText = inputNombre.value;
   if(inputNombre.value.length === 0) {
@@ -43,17 +42,17 @@ inputNombre.addEventListener("input", () => {
 
 nombre.addEventListener("input", function(){
   let soloNom = this.value;
-  soloNom = soloNom.replace(/[^a-zA-Z]/g,"")
+  soloNom = soloNom.replace(/[^a-zA-Z]/g," ")
   this.value = soloNom;
 });
 
 
 
 // ingresar dentro de mi tarjeta el maskify y solo campos numericos en input de numero de tarjeta
-  inputTarjeta.addEventListener("input", () => {
+inputTarjeta.addEventListener("input", () => {
   cardNumber.innerText = inputTarjeta.value;
   function maskify(input) {
-    return input.slice(0, -4).replace(/./g, '#') + input.slice(-4)
+    return input.slice(0, -4).replace(/./g, '#') + input.slice(-4) // maskify
   }
   const originalTexto = parrafo.textContent;
   const maskedTexto = maskify(originalTexto);
@@ -78,6 +77,7 @@ inputMes.addEventListener("input", () => {
   if(inputMes.value.length === 0) {
     cardMes.innerText ="00"
   }
+  
 })
 
 mesInput.addEventListener("input", function(){
@@ -113,23 +113,24 @@ inputCvv.addEventListener("input", () => {
 
 //Algoritmo de Luhn
 function validarTarjeta(numeroTarjeta) {
-    let suma = 0;
-    let multiplicador = 1;
-     for (let i = numeroTarjeta.length - 1; i >= 0; i--) {  // Recorrer los dígitos de la tarjeta de crédito de derecha a izquierda
-    let digito = parseInt(numeroTarjeta.charAt(i));
+  let suma = 0; // la suma parte de 0 
+  let multiplicador = 1; // osea posicion 1
+     
+  for (let i = numeroTarjeta.length - 1; i >= 0; i--) {  // Recorrer los dígitos de la tarjeta de crédito de derecha a izquierda // leng vcuenta posiciones que tienes 
+    let digito = parseInt(numeroTarjeta.charAt(i)); //parseINT te modifica el texto a un numero
     
-    if (multiplicador % 2 == 0) {   // Multiplicar los dígitos pares por 2
-      digito *= 2;
+    if (multiplicador % 2 === 0) {   // aqui verifico si el multiplicador es uno o dos si es 1 no entra al if 
+      digito *= 2; //si es 2 lo multiplica por dos
      
       if (digito > 9) {  // Si el resultado de la multiplicación es mayor a 9, sumar los dígitos del número resultante
-        digito -= 9;
+        digito -= 9; // men vez de suma le resto 9 que da lo mismo
       }
     } 
+    multiplicador = multiplicador === 1 ? 2 : 1;   // Cambiar el multiplicador para alternar entre 1 y 2
     suma += digito;   // Sumar el dígito actual a la suma total
-    multiplicador = multiplicador == 1 ? 2 : 1;   // Cambiar el multiplicador para alternar entre 1 y 2
   }
-  return suma % 10 == 0;   // La tarjeta de crédito es válida si la suma total es un múltiplo de 10
-} 
+  return suma % 10 === 0;   // La tarjeta de crédito es válida si la suma total es un múltiplo de 10
+}
 
 
 
@@ -137,38 +138,25 @@ function validarTarjeta(numeroTarjeta) {
 
 
 // Evento de escuchar o llamar al botón para validar tarjeta
-    verificarTarjetaButton.addEventListener('click', function() 
-    {
-    const esValida = validarTarjeta(numeroTarjetaInput.value);
-   if(numeroTarjetaInput.value == "" || nombre.value == "" )
-        {
-            alert('Rellene todos los campos');
-        }
- else{
+verificarTarjetaButton.addEventListener('click', function() 
+{
+  const esValida = validarTarjeta(numeroTarjetaInput.value);
+  if(numeroTarjetaInput.value === "" || nombre.value === "" )
+  {
+    alert('Rellene todos los campos');
+  }
+  else{
     if (esValida) 
     {        
-        alert('Tarjeta Valida!');
-        document.getElementById("gracias").style.display ='block' 
-        document.getElementById("contenedor").style.display ='none' // debo cambiar el alert por un mensaje en pantalla (con otro html?)
-        document.getElementById("tarjeta").style.display ="none"
+      alert('Tarjeta Valida!');
+      document.getElementById("gracias").style.display ='block' 
+      document.getElementById("contenedor").style.display ='none' // debo cambiar el alert por un mensaje en pantalla (con otro html?)
+      document.getElementById("tarjeta").style.display ="none"
     } 
     else 
     {
-        alert('Ingrese un número de tarjeta valido');
+      alert('Ingrese un número de tarjeta valido');
     }
   }
 }
-,);
-
-
-
-
-
-
-
-
-
-/*.replace(/\s/g, "")
-  .replace(/\D/g, "")
-  .replace(/([0-9]{4})/g, "$1 ");*/
-
+  ,);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,10 @@
 const validator = {
-  // ...
+  
 };
 
 export default validator;
+
+
+
+
+
